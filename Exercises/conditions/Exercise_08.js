85836cc47f1d96763b2619507c957111697fc8e9
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-02-03 02:43:55 +0100

    Commit change

--- Exercises/conditions/Exercise_08.js
+++ Exercises/conditions/Exercise_08.js
@@ -4,6 +4,7 @@
  * If both conditions are true, print "mirror image" of given number.
  * If it is not two-digit number, return the right information.
  */
+
 var num = 25;   // 25, 31, 'a'
 var result;
 
@@ -11,7 +12,7 @@ if(typeof num === 'number') {
     if(num > 9 && num < 100) {
         var digit;
         digit = num % 10;
-        result = digit *10;
+        result = digit * 10;
         result += (num - digit) / 10;
     } else {
         result = num + ' is not two-digit number.';
